93e7325cd5c84bc1a595a3ce2b162c437ee9d1ad
Timileyin Akinlaja <timileyin.akinlaja@vigilearn.com>
2026-03-16 12:38:41 +0100

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [
     react(),
     tailwindcss(),
   ],
+  base: '/', // <--- IMPORTANT: use '/' if deployed to root domain 
   build: {
     outDir: 'dist'
   }
