3f2c3e0e84dc8bb0555ddfa4531e28c79f5812f0
Timileyin Akinlaja <timileyin.akinlaja@vigilearn.com>
2026-03-17 12:08:13 +0100

    enabled pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'BidsnBuy - Bid. Win. Buy Now.',
+        short_name: 'BidsnBuy',
+        description: 'Nigeria\'s premier destination for both thrill-seekers and direct shoppers.',
+        theme_color: '#1e5cea',
+        icons: [
+          {
+            src: 'web-app-manifest-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
   ],
   base: '/', // <--- IMPORTANT: use '/' if deployed to root domain 
   build: {
